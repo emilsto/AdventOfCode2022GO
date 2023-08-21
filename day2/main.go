c2e9ab38cb300e14113d6d9f6da3a9eafd834351
Emil <emil.kantaneva@gmail.com>
2023-08-21 23:01:30 +0300

    compelte day2 pt1

--- day2/main.go
+++ day2/main.go
@@ -2,21 +2,27 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
 )
 
+type Move byte
+
 const (
-	A int = 0
-	B     = 1
-	C     = 2
-	X     = 3
-	Y     = 4
-	Z     = 5
+	RockOpponent     Move = 'A'
+	PaperOpponent    Move = 'B'
+	ScissorsOpponent Move = 'C'
+	RockPlayer       Move = 'X'
+	PaperPlayer      Move = 'Y'
+	ScissorsPlayer   Move = 'Z'
 )
 
-func readFileToScanner(path string, buf *[]string) error {
+type Turn struct {
+	playerMove   Move
+	opponentMove Move
+}
+
+func readFileToBuf(path string, buf *[]Turn) error {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
@@ -26,8 +32,21 @@ func readFileToScanner(path string, buf *[]string) error {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		line := scanner.Text()
-		*buf = append(*buf, line)
+		line := scanner.Bytes()
+
+		// We trust that the moves are 1. Player and 2. Opponent and that
+		// They are space separated
+		if len(line) == 3 {
+			oMove := Move(line[0])
+			pMove := Move(line[2])
+			turn := Turn{
+				playerMove:   pMove,
+				opponentMove: oMove,
+			}
+			*buf = append(*buf, turn)
+		}
+		// If the line is not len of 3, then it is fucked
+		continue
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -40,14 +59,56 @@ func readFileToScanner(path string, buf *[]string) error {
 
 func main() {
 
-	var buffer []string
+	var turns []Turn
 
-	err := readFileToScanner("moves.txt", &buffer)
+	err := readFileToBuf("moves.txt", &turns)
 
 	if err != nil {
 		log.Fatal("Error in reading file")
 	}
 
-	fmt.Println(len(buffer))
+	var totalPointage int16 = 0
 
+	for i := range turns {
+		totalPointage += getPoints(turns[i])
+	}
+
+	println("Total pointage :", totalPointage)
+
+}
+
+func getPoints(turn Turn) int16 {
+	var baseLinePoints int16 = 0
+	switch turn.playerMove {
+	case RockPlayer:
+		baseLinePoints += 1
+		// Draw case
+		if turn.opponentMove == RockOpponent {
+			baseLinePoints += 3
+		} else if turn.opponentMove == ScissorsOpponent {
+			baseLinePoints += 6
+		} else {
+			baseLinePoints += 0
+		}
+		return baseLinePoints
+	case PaperPlayer:
+		baseLinePoints += 2
+		if turn.opponentMove == PaperOpponent {
+			baseLinePoints += 3
+		} else if turn.opponentMove == RockOpponent {
+			baseLinePoints += 6
+		} else {
+			baseLinePoints += 0
+		}
+	case ScissorsPlayer:
+		baseLinePoints += 3
+		if turn.opponentMove == ScissorsOpponent {
+			baseLinePoints += 3
+		} else if turn.opponentMove == PaperOpponent {
+			baseLinePoints += 6
+		} else {
+			baseLinePoints += 0
+		}
+	}
+	return baseLinePoints
 }
