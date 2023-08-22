f979b5c83b071fc2bb6d27208f73120ebdcaa4af
Emil <emil.kantaneva@gmail.com>
2023-08-22 22:55:41 +0300

    change variable names to be more descriptive

--- day3/main.go
+++ day3/main.go
@@ -9,8 +9,8 @@ import (
 type compartment string
 
 type Rucksack struct {
-	first  compartment
-	second compartment
+	first_compartment  compartment
+	second_compartment compartment
 }
 
 func readFileToBuf(path string, buf *[]string) error {
@@ -44,8 +44,8 @@ func main() {
 	var score int = 0
 	for i := 0; i < len(buf); i++ {
 		sack := createRucksack(&buf[i])
-		points := matchItems(&sack)
-		score += (scoreMatches(&points, &scoremap))
+		mathched_compartment_types := matchItems(&sack)
+		score += (scoreMatches(&mathched_compartment_types, &scoremap))
 	}
 
 	print(score)
@@ -65,34 +65,35 @@ func generateScoreMap() map[string]int {
 
 func createRucksack(buf *string) Rucksack {
 	var rucksack Rucksack
-	// We know that all rucksacks are devisible by 2
+	// We know (belive) that all rucksacks are devisible by 2
 	halfpoint := (len(*buf) / 2)
-	rucksack.first = compartment((*buf)[0:halfpoint])
-	rucksack.second = compartment((*buf)[halfpoint:])
+	rucksack.first_compartment = compartment((*buf)[:halfpoint])
+	rucksack.second_compartment = compartment((*buf)[halfpoint:])
 
 	return rucksack
 }
 
 func matchItems(rucksack *Rucksack) map[string]int {
-	match := make(map[string]int)
+	matched_types := make(map[string]int)
 
-	for i := range rucksack.first {
-		for j := range rucksack.second {
-			if rucksack.first[i] == rucksack.second[j] {
-				_, found := match[string(rucksack.first[i])]
+	// Making two maps would be a way to make this O(n) instead of O(n^2)
+	for i := range rucksack.first_compartment {
+		for j := range rucksack.second_compartment {
+			if rucksack.first_compartment[i] == rucksack.second_compartment[j] {
+				_, found := matched_types[string(rucksack.first_compartment[i])]
 				if !found {
-					match[string(rucksack.first[i])] = i
+					matched_types[string(rucksack.first_compartment[i])] = i
 				}
 			}
 		}
 	}
-	return match
+	return matched_types
 }
 
-func scoreMatches(match *map[string]int, scores *map[string]int) int {
+func scoreMatches(mathed_types *map[string]int, scores *map[string]int) int {
 
 	var result int
-	for key := range *match {
+	for key := range *mathed_types {
 		result = (*scores)[key]
 	}
 
